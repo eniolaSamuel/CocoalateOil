95e2ae40d9eb3237208681a5d34fd41df4a5892e
Eniola Samuel <theeniolasamuel@gmail.com>
2024-03-03 16:31:40 +0100

    commit contact start

---
+++ WebstormProjects/cocoalate_hair_oil/src/views/ContactUs.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/ContactUs.css"
+
+export const ContactUs = () => {
+    return(
+        <div className="contact-main-frame">
+            <div className="contact-sub-frame">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
