e48abd82e5eddb223ed2fdb2cdcf05188e111765
Eniola Samuel <theeniolasamuel@gmail.com>
2024-03-07 20:21:10 +0100

    commit faq

--- WebstormProjects/cocoalate_hair_oil/src/views/Footer.jsx
+++ WebstormProjects/cocoalate_hair_oil/src/views/Footer.jsx
@@ -1,8 +1,9 @@
 export const Footer = () => {
     return(
         <div>
-            <p>"CocoaLate Crown Hair Growth Elixirs: Nourishing Roots, Growing Dreams.
-                Elevate your hair care routine with our premium blends infused with nature's finest ingredients.
+            <p>"CocoaLate Crown Hair Growth Elixirs: Nourishing Roots, Growing Dreams.</p>
+
+            <p>Elevate your hair care routine with our premium blends infused with nature's finest ingredients.
                 For inquiries, partnerships, or custom orders, contact us at 'cocoalate0@gmail.com' or +2347080772782.
                 Follow us on instagram @cocoalate_crown_craft, for hair care tips, inspiration, and exclusive offers.
                 © 2024 CocoaLate Crown. All rights reserved."</p>
