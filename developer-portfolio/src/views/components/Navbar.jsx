1896a978902287fb01202715122726f96513858a
Eniola Samuel <theeniolasamuel@gmail.com>
2024-03-18 00:05:57 +0100

    let us begin

--- developer-portfolio/src/views/components/Navbar.jsx
+++ developer-portfolio/src/views/components/Navbar.jsx
@@ -1,16 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../../assets/svg/monogram-logo.svg'
+import "../../styles/components/Navbar.css"
+
 
 const Navbar = () => {
+
+    let links = [
+        { name: "ABOUT", link: "/" },
+        { name: "PROJECT", link: "/" },
+        { name: "CONTACTS", link: "/" },
+    ];
+
     return(
         <div className="nav-main-frame">
-            <div className="logo-frame">
-                <img src={Logo}
-                     alt={Logo}
-                     className="logo-image"/>
+            <img src={Logo}
+                 alt={Logo}
+                 className="logo-image"/>
+            <div className="nav-links-frame">
+
+                <ul className="nav-link">
+                    {links.map((link) => (
+                        <li key={link.name} className="nav-link-detail">
+                            <a href={link.link}>{link.name}</a>
+                        </li>
+                    ))}
+                </ul>
             </div>
-            <div className="nav-links-frame"></div>
         </div>
     )
 }
