fe0da0c2d4538033663a0a45b0b74637b346afef
Eniola Samuel <theeniolasamuel@gmail.com>
2024-03-19 19:27:15 +0100

    let us begin

---
+++ WebstormProjects/cocoalate_hair_oil/src/views/Directions.jsx
@@ -0,0 +1,42 @@
+const Directions= ()=>{
+    return(
+        <div className="directions-main-frame">
+            <p>
+                Preparation:
+
+                Before using the hair growth oil, ensure that your hair and scalp are clean and free from any styling products or buildup. You can wash your hair with a gentle shampoo and towel dry it lightly.
+                Application:
+
+                Shake the bottle of hair growth oil well to ensure all ingredients are evenly mixed.
+                Using the dropper provided or your fingertips, apply a small amount of oil directly onto your scalp.
+                Part your hair into sections to ensure even distribution of the oil.
+                Gently massage the oil into your scalp using circular motions for 5-10 minutes to promote blood circulation and stimulate hair follicles.
+                If desired, apply a small amount of oil to the lengths and ends of your hair for added nourishment and shine.
+                Leave-In Treatment:
+
+                After massaging the oil into your scalp and hair, leave it in overnight for maximum absorption and benefits.
+                For a more intensive treatment, cover your hair with a shower cap or wrap it in a warm towel to help the oil penetrate deeply into the hair shaft.
+                Washing Out:
+
+                In the morning, wash your hair thoroughly with a gentle shampoo to remove any excess oil.
+                Follow up with conditioner as usual to keep your hair soft and manageable.
+                Frequency:
+
+                For best results, use the hair growth oil 2-3 times per week as part of your regular hair care routine.
+                Consistency is key, so continue using the oil regularly to see visible improvements in hair growth and health over time.
+                Caution:
+
+                Avoid contact with eyes. If contact occurs, rinse thoroughly with water.
+                Perform a patch test before using the oil to check for any allergic reactions or skin sensitivities.
+                Discontinue use if irritation occurs and consult a healthcare professional if necessary.
+                Storage:
+
+                Store the hair growth oil in a cool, dry place away from direct sunlight.
+                Keep the bottle tightly closed when not in use to prevent oxidation and maintain the quality of the oil.
+                Disclaimer:
+
+                This hair growth oil is intended for external use only. Avoid ingestion and keep out of reach of children.
+            </p>
+        </div>
+    )
+}
