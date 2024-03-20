604cedae6a0adf5325c580d74a2614380b69c6a5
Eniola Samuel <theeniolasamuel@gmail.com>
2024-03-20 22:31:12 +0100

    hair application direction update

--- WebstormProjects/cocoalate_hair_oil/src/views/Directions.jsx
+++ WebstormProjects/cocoalate_hair_oil/src/views/Directions.jsx
@@ -18,25 +18,30 @@ const Directions= ()=>{
                 Leave-In Treatment:
                 After massaging the oil into your scalp and hair, leave it in overnight for maximum absorption and benefits.
                 For a more intensive treatment, cover your hair with a shower cap or wrap it in a warm towel to help the oil penetrate deeply into the hair shaft.
-                Washing Out:
+                <hr/>
 
+                Washing Out:
                 In the morning, wash your hair thoroughly with a gentle shampoo to remove any excess oil.
                 Follow up with conditioner as usual to keep your hair soft and manageable.
-                Frequency:
+                <hr/>
 
+                Frequency:
                 For best results, use the hair growth oil 2-3 times per week as part of your regular hair care routine.
                 Consistency is key, so continue using the oil regularly to see visible improvements in hair growth and health over time.
-                Caution:
+                <hr/>
 
+                Caution:
                 Avoid contact with eyes. If contact occurs, rinse thoroughly with water.
                 Perform a patch test before using the oil to check for any allergic reactions or skin sensitivities.
                 Discontinue use if irritation occurs and consult a healthcare professional if necessary.
-                Storage:
+                <hr/>
 
+                Storage:
                 Store the hair growth oil in a cool, dry place away from direct sunlight.
                 Keep the bottle tightly closed when not in use to prevent oxidation and maintain the quality of the oil.
-                Disclaimer:
+                <hr/>
 
+                Disclaimer:
                 This hair growth oil is intended for external use only. Avoid ingestion and keep out of reach of children.
             </p>
         </div>
